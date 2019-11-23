c2046684e206db52cfaddeaf2f09e7817e330ded
Alex Qiao <42728108+aqiao18@users.noreply.github.com>
2019-11-23 14:19:35 -0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:tiger@localhost/beers'
-
-SQLALCHEMY_BINDS = {
-    'devils_abroad': 'postgresql://postgres:tiger@localhost/devils_abroad'
-}
-
-SQLALCHEMY_ECHO = True
-DEBUG = True
