67028220fb123ee6f7446e7f2cab57038185fc24
ddchen27 <davidchen1337@gmail.com>
2019-12-09 09:09:38 -0500

    final create.sql

--- milestone2/create.sql
+++ milestone2/create.sql
@@ -37,19 +37,9 @@ course_name VARCHAR(100) NOT NULL,
 program_name VARCHAR(100) NOT NULL,
 FOREIGN KEY (program_name) REFERENCES Program(program_name));
 
---CREATE TABLE AbroadUser
---(u_email VARCHAR(100) NOT NULL,
---term VARCHAR(100) NOT NULL,
---program_name VARCHAR(100) NOT NULL,
---PRIMARY KEY (u_email, term, program_name),
---UNIQUE (u_email, term),
---FOREIGN KEY (u_email) REFERENCES Users(email),
---FOREIGN KEY (program_name) REFERENCES Program(program_name));
-
 CREATE TABLE Review
 (id VARCHAR(100)  NOT NULL PRIMARY KEY,
--- uuid UUID NOT NULL DEFAULT uuid_generate_v1() PRIMARY KEY,
-country VARCHAR(100) NOT NULL, --fix later, doesn't reference countries!
+country VARCHAR(100) NOT NULL,
 duke_major_code VARCHAR(100) NOT NULL REFERENCES MajorCodes(duke_major_code),
 course_uuid UUID NOT NULL,
 u_email VARCHAR(100) NOT NULL,
@@ -59,29 +49,9 @@ difficulty FLOAT NOT NULL CHECK (difficulty >= 0 AND difficulty <= 5.0),
 UNIQUE(u_email, course_uuid),
 FOREIGN KEY (course_uuid) REFERENCES Course(uuid));
 
--- CREATE TABLE Likes
--- (u_email VARCHAR(100) NOT NULL,
--- review_id UUID NOT NULL,
--- PRIMARY KEY (u_email, review_id),
--- FOREIGN KEY (u_email) REFERENCES Users(email),
--- FOREIGN KEY (review_id) REFERENCES Review(id) ON DELETE CASCADE);
-
-
 -- TRIGGERS
-
---CREATE FUNCTION TS_No_edit_own_review() RETURNS TRIGGER AS $$ 
---BEGIN
---	IF (NEW.u_email <> Review.u_email) FROM Review THEN
---		RAISE EXCEPTION 'Cannot update or delete reviews that you didn’t write';
---	END IF;
---	RETURN NEW;
---END;
---$$ LANGUAGE plpgsql;
-
---CREATE TRIGGER Edit_Own_Review
---	BEFORE UPDATE OR DELETE ON Review
---	FOR EACH ROW
---	EXECUTE PROCEDURE TS_No_edit_own_review();
+-- Removed many of the triggers from previous versions of the app in favor of testing for error violations on the backend
+-- and inaccessible webpages (@login_required and hidden webpages)
 
 CREATE FUNCTION TS_No_phantom() RETURNS TRIGGER AS $$
 BEGIN
@@ -98,21 +68,7 @@ CREATE TRIGGER No_Phantom_Reviews
 	FOR EACH ROW
 	EXECUTE PROCEDURE TS_No_phantom();
 
---CREATE FUNCTION TS_No_fake_reviews() RETURNS TRIGGER AS $$
---BEGIN
---	IF (NEW.program_name NOT IN (SELECT program_name FROM Users WHERE email = NEW.u_email)) THEN
---	RAISE EXCEPTION 'Cannot be writing a review for a course in a program you have not been in';
---	END IF;
---	RETURN NEW;
---END;
---$$ LANGUAGE plpgsql;
-
---CREATE TRIGGER TS_No_fake_reviews
---	BEFORE INSERT ON Review
---	FOR EACH ROW EXECUTE PROCEDURE TS_No_fake_reviews();
-
 -- Indexes
-
 CREATE INDEX Get_User
 on Users (email, name);
 
@@ -138,7 +94,3 @@ INSERT INTO Review VALUES ('1', 'Germany', 'COMPSCI', (SELECT uuid from Course W
 INSERT INTO Review VALUES ('2', 'Spain', 'COMPSCI', (SELECT uuid from Course WHERE duke_code='COMPSCI 300' and course_name='Analysis of Big Data' and program_name='Duke in Madrid'), 'mr328@duke.edu', 'I think this class SUCKS!', 1, 5);
 INSERT INTO Review VALUES ('3', 'Australia','COMPSCI', (SELECT uuid from Course WHERE duke_code='COMPSCI 250' and course_name='Computer Architecture' and program_name='University of New South Wales'), 'smw81@duke.edu', 'I think this rocks hehe!', 5, 1);
 INSERT INTO Review VALUES('4', 'Australia','COMPSCI', (SELECT uuid from Course WHERE duke_code='COMPSCI 300' and course_name='Networks' and program_name='University of New South Wales'), 'smw81@duke.edu', 'amazing!', 5, 3);
-
--- TESTING TRIGGERS
-
--- INSERT INTO Review VALUES ("4", 'Duke in Madrid', 'CS 300', 'Networks' , 'ddc27@duke.edu', 'hahaha i didn’t even take this class!', 4.5, 2.0);
